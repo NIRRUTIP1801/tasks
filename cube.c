f7d4a4eb8673ba89dd18027bc34ce12c1e1b41f7
NIRRUTIP1801 <Nirruti@gmail.com>
2025-03-02 09:23:30 +0000

    update comments

--- cube.c
+++ cube.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int cube(int n);
+int cube(int n); //declaring function to calculate cube
 
-int main(){
-  int n;
+int main(){      
+  int n;         //variable to store value
   
-  printf("enter value of number:");
-  scanf("%d",&n);
-  printf("cube of a no. is:%d",cube(n));
+  printf("enter value of number:");      //asking user to input a number
+  scanf("%d",&n);                        //taking input n
+  printf("cube of a no. is:%d",cube(n));  //printing cube by calling a function
   
- return 0;
+ return 0;                    //returnig 0
 }
 
-int cube(int n){
-  int cube=n*n*n;
-  return cube;
+int cube(int n){                 //definig function cube
+  int cube=n*n*n;                 //calculating cube
+  return cube;               //returning 
 }
 
 
--- per.c
+++ per.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-int prct(int a,int b);
+int prct(int a,int b);   //declaring function to calculate perimeter
 
 int main(){
-  int a,b;
+  int a,b;                        //variable to store value of a and b
   
-  printf("enter value of l an b:");
-  scanf("%d%d",&a,&b);
-  printf("perimeter of rectangle is:%d",prct(a,b));
+  printf("enter value of l an b:");                           //asking for values of a &b
+  scanf("%d%d",&a,&b);                                       //taking input a &b
+  printf("perimeter of rectangle is:%d",prct(a,b));              //printing perimeter by calling a function
   
  return 0;
 }
 
-int prct(int a,int b){
-  int prim=2*(a+b);
-  return prim;
+int prct(int a,int b){               //definig function cube
+  int prim=2*(a+b);                     //calculating perimeter of rectangle 
+  return prim;                           //returning perimetr
 }//end
